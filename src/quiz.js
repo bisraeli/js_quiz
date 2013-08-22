ce480c47d53763198a84097e14a4b0de20ff3b61
Ben Israeli <jisraeli@gmail.com>
2013-08-22 10:05:42 -0400

    It's a shame we haven't gone over these

--- src/quiz.js
+++ src/quiz.js
@@ -4,6 +4,10 @@
 //      |  |     |   __   | |  |     \   \       |  |     \   \         /  /_\  \      |  |  |  |   |  |  |  | |  |    /  /
 //      |  |     |  |  |  | |  | .----)   |      |  | .----)   |       /  _____  \     |  `--'  '--.|  `--'  | |  |   /  /----.__
 //      |__|     |__|  |__| |__| |_______/       |__| |_______/       /__/     \__\     \_____\_____\\______/  |__|  /________(__)
+
+I spent most of quiz time reading up on testing with jasmine so I could complete
+quiz later on my own time.
+
 var quiz = {};
 
 // Question 1: a function called sleepIn
@@ -12,6 +16,10 @@ var quiz = {};
 // quiz.sleepIn() => false
 // quiz.sleepIn({vacation: true}) => true
 quiz.sleepIn = function(options) {
+  var d = Date.today
+  var n = d.getDay();
+  if (n === Saturday || n === Sunday)
+    return true
 }
 
 // Question 2: function called nearHundred
@@ -21,6 +29,12 @@ quiz.sleepIn = function(options) {
 // quiz.nearHundred(52) => false
 // quiz.nearHundred(93) => false
 // quiz.nearHundred('two') => Error: Please enter a number!
+quiz.nearHundred = function(number){
+  if (number >== 90){
+    return true
+}else {
+    return false
+}
 
 // Question 3: a function called missingChar
 //////////////
@@ -29,14 +43,24 @@ quiz.sleepIn = function(options) {
 // quiz.missingChar("kittie", 1) => "kttie"
 // quiz.missingChar(347, 1) => Error: Please enter a string!
 
+quiz.missingChar = function(string, index){
+  string.slice()
+
+}
+
 // Question 4: a function called delDel
 //////////////
 // Remove "del" from a string.
 // quiz.delDel("abdelcd") => "abcd"
 // quiz.delDel("xyz") => "xyz"
-
+quiz.delDel = function(string){
+  string.delete ("del")
+}
 // Question 5: a method called backAround
 //////////////
 // Given a string, move the last character to the beginning.
 // "cat".backAround() => "tca"
 // "hello".backAround() => "ohell"
+quiz.backAround(string) = function(string){
+
+}
